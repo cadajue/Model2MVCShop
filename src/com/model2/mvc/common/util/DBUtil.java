8ea1331946d4f3566d3c61a0936bcb72882ec356
SonGeon <cadajue@gmail.com>
2020-05-11 20:06:07 +0900

    mybatis 임시처리

--- src/com/model2/mvc/common/util/DBUtil.java
+++ src/com/model2/mvc/common/util/DBUtil.java
@@ -1,8 +1,15 @@
 package com.model2.mvc.common.util;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 
 public class DBUtil {
@@ -28,4 +35,19 @@ public class DBUtil {
 		}
 		return conn;
 	}
+	
+	public static SqlSession getSqlSession() throws IOException {
+		//==> 1. xml metadata 읽는 Stream 생성
+		Reader reader = Resources.getResourceAsReader("com/model2/mvc/resources/sql/mybatis-config.xml");
+		
+		//==> 2. Reader 객체를 이용 xml MetaData 에 설정된 각정 정보를 접근, 사용가능한 
+		//==>     SqlSession을 생성하는 SqlSessionFactory  instance 생성
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		//==>3. SqlSessionFactory 를 통해 autoCommit true 인 SqlSession instance 생성
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		
+		return sqlSession;
+	}
+	
+	
 }
